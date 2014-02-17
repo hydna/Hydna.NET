fab35b8015ed7f526789272c80c5b4a694f1ea10
Johan Dahlberg <dahlberg.johan@gmail.com>
2014-02-17 16:47:31 +0100

    Simplified example

--- src/Hydna.Net.Examples/HelloWorld.cs
+++ src/Hydna.Net.Examples/HelloWorld.cs
@@ -7,43 +7,38 @@ namespace Hydna.Net.Examples.HelloWorld
 {
     static class HelloWorld
     {
+        static Channel channel;
+
         public static void Main(string[] args) 
         {
-            Channel channel = new Channel();
+            channel = new Channel();
+
+            channel.Connect("http://public.hydna.net/", ChannelMode.ReadWrite);
 
             channel.Open += c_Open;
             channel.Data += c_Data;
-            channel.Closed += delegate(object sender, ChannelCloseEventArgs e) {
-                Console.WriteLine("Channel is now closed, reason: " + e.Reason);
-            };
-            
-            channel.Connect("https://testing.hydna.net/", ChannelMode.ReadWrite);
+            channel.Closed += c_Closed;
 
-            Channel channel2 = new Channel();
-            channel2.Open += c_Open;
-            channel2.Connect("publdasdsa.hydna.net/notwo?lol", ChannelMode.ReadWrite);
-
-            while (channel.State != ChannelState.Closed)
-                Thread.SpinWait(10000000);
+            while (channel != null)
+                Thread.SpinWait(1);
         }
 
         static void c_Open(object sender, ChannelEventArgs e)
         {
             Console.WriteLine("Channel is now open: " + e.Text);
-            Channel channel = (Channel) sender;
             channel.Send("Hello world");
         }
 
         static void c_Data(object sender, ChannelEventArgs e)
         {
             Console.WriteLine("Received data: " + e.Text);
-            Channel channel = (Channel) sender;
             channel.Close();
         }
 
         static void c_Closed(object sender, ChannelCloseEventArgs e)
         {
-            Console.WriteLine("Channel is now closed");
+            Console.WriteLine("Channel is now closed, bye..");
+            channel = null;
         }
     }
 }
